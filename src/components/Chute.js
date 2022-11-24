d238931c06f596808f13b4f6ccc8afa1e19144e0
drebraga <dre.braga@gmail.com>
2022-11-24 03:52:01 -0300

    v1.7 Desabilitar botões de resposta corrigido

--- src/components/Chute.js
+++ src/components/Chute.js
@@ -4,8 +4,8 @@ function Chute({gameStart}) {
         <>
             <div className="chute">
                 <p>Já sei a palavra!</p>
-                <input data-test="guess-input" disabled={gameStart} type="text" placeholder="" />
-                <input data-test="guess-button" disabled={gameStart} type="button" value="Chutar" />
+                <input data-test="guess-input" disabled={!gameStart} type="text" placeholder="" />
+                <input data-test="guess-button" disabled={!gameStart} onClick={() => alert("Chutei")} type="button" value="Chutar" />
             </div>        
         </>
     )
